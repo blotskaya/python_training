b0aecd765e3d1a636e06148481b4e883f694adb1
a.blotskaya <blotskaya.arina@gmail.com>
2022-10-12 20:46:52 +0300

    Улучшение теста создания новой группы

--- test_add_group_2.py
+++ test_add_group_2.py
@@ -10,34 +10,32 @@ class UntitledTestCase(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/index.php")
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
 
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
         wd.get("http://localhost/addressbook/group.php")
 
-    def create_group(self, wd):
+    def create_group(self, wd, groupname, groupheader, groupfooter):
         #init group creation
         wd.find_element_by_name("new").click()
         #fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("group_name")
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("group_name1")
+        wd.find_element_by_name("group_name").send_keys(groupname)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("group_header2")
+        wd.find_element_by_name("group_header").send_keys(groupheader)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("group_footer3")
+        wd.find_element_by_name("group_footer").send_keys(groupfooter)
         #submit group creation
         wd.find_element_by_name("submit").click()
 
@@ -50,9 +48,9 @@ class UntitledTestCase(unittest.TestCase):
     def test_untitled_test_case(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, groupname="group_name1", groupheader="group_header2", groupfooter="group_footer3")
         self.return_to_groups_page(wd)
         self.logout(wd)
 
