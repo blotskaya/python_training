f6245333b9b47c83b4f77e96614c6d9a7fc0f8c8
a.blotskaya <blotskaya.arina@gmail.com>
2022-10-17 18:31:43 +0300

    Исправлены названия переменных

--- contact.py
+++ contact.py
@@ -1,30 +1,30 @@
 class Contact:
-    def __init__(self, contactname, contactmiddlename, contactsurname, contactnickname, contacttitle, contactcompany,
-                 contactaddress,contacthome, contactmobile, contactwork, contactfax, contactmail1, contactmail2,
-                 contactmail3, contacthp, contactbyear, contactayear, contactaddress2, contacthome2, contactnotes,
-                 contactbday, contactbmonth, contactaday, contactamonth):
-        self.contactname = contactname
-        self.contactmiddlename = contactmiddlename
-        self.contactsurname = contactsurname
-        self.contactnickname = contactnickname
-        self.contacttitle = contacttitle
-        self.contactcompany = contactcompany
-        self.contactaddress = contactaddress
-        self.contacthome = contacthome
-        self.contactmobile = contactmobile
-        self.contactwork = contactwork
-        self.contactfax = contactfax
-        self.contactmail1 = contactmail1
-        self.contactmail2 = contactmail2
-        self.contactmail3 = contactmail3
-        self.contacthp = contacthp
-        self.contactbyear = contactbyear
-        self.contactayear = contactayear
-        self.contactaddress2 = contactaddress2
-        self.contacthome2 = contacthome2
-        self.contactnotes = contactnotes
-        self.contactbday = contactbday
-        self.contactaday = contactaday
-        self.contactbmonth = contactbmonth
-        self.contactamonth = contactamonth
+    def __init__(self, name, middlename, surname, nickname, title, company,
+                 address, phonehome, phonemobile, phonework, phonefax, mail1, mail2,
+                 mail3, hp, byear, ayear, address2, home2, notes,
+                 bday, bmonth, aday, amonth):
+        self.name = name
+        self.middlename = middlename
+        self.surname = surname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.phonehome = phonehome
+        self.phonemobile = phonemobile
+        self.phonework = phonework
+        self.phonefax = phonefax
+        self.mail1 = mail1
+        self.mail2 = mail2
+        self.mail3 = mail3
+        self.hp = hp
+        self.byear = byear
+        self.ayear = ayear
+        self.address2 = address2
+        self.home2 = home2
+        self.notes = notes
+        self.bday = bday
+        self.aday = aday
+        self.bmonth = bmonth
+        self.amonth = amonth
 
