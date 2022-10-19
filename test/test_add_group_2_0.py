9d12b546cb10b94d543776da3bc678873b6b9215
a.blotskaya <blotskaya.arina@gmail.com>
2022-10-19 19:32:10 +0300

    Создана общая фикстура для всех тестов

--- test/test_add_group_2_0.py
+++ test/test_add_group_2_0.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.group import Group
-from fixture.application import Application
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-    
 def test_untitled_test_case(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(groupname="group_name1", groupheader="group_header2", groupfooter="group_footer3"))
