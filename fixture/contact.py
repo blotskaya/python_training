988c4feb64956de0407e5e4f468dc1bfc7438f29
a.blotskaya <blotskaya.arina@gmail.com>
2022-10-26 16:36:32 +0300

    В тестах контактов добавлен переход на страницу контактов. Создан отдельный метод заполнения полей групп и контактов в соответствующих фикстурах групп и контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -8,79 +8,15 @@ class ContactHelper:
         # init contact creation
         wd.find_element_by_link_text("add new").click()
         wd.get("http://localhost/addressbook/edit.php")
-        # fill contact forms
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.name)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.surname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.phonehome)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.phonemobile)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.phonework)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.phonefax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.mail1)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.mail2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.mail3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.hp)
-        wd.find_element_by_css_selector("select[name=\"bday\"] > option[value=%s]" % contact.bday).click()
-        wd.find_element_by_css_selector("select[name=\"bmonth\"] > option[value=%s]" % contact.bmonth).click()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        wd.find_element_by_css_selector("select[name=\"aday\"] > option[value=%s]" % contact.aday).click()
-        wd.find_element_by_css_selector("select[name=\"amonth\"] > option[value=%s]" % contact.amonth).click()
-        wd.find_element_by_name("ayear").send_keys(contact.ayear)
-        wd.find_element_by_name("new_group").click()
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contact.home2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact.notes)
+        self.fill_contact_form(contact)
         # submit changes
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
         self.return_to_contacts_page()
 
     def delete_first_contact(self):
         wd = self.app.wd
+        # open contacts page
+        wd.get("http://localhost/addressbook/index.php")
         #select contact
         wd.find_element_by_name("selected[]").click()
         #delete first contact
@@ -89,8 +25,17 @@ class ContactHelper:
 
     def edit_first_contact(self, contact):
         wd = self.app.wd
+        #open contacts page
+        wd.get("http://localhost/addressbook/index.php")
         #select contact
         wd.find_element_by_css_selector("img[alt=\"Edit\"]").click()
+        self.fill_contact_form(contact)
+        #submit
+        wd.find_element_by_name("update").click()
+        self.return_to_contacts_page()
+
+    def fill_contact_form(self, contact):
+        wd = self.app.wd
         # fill contact forms
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
@@ -154,9 +99,6 @@ class ContactHelper:
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys(contact.notes)
-        #submit
-        wd.find_element_by_name("update").click()
-        self.return_to_contacts_page()
 
     def return_to_contacts_page(self):
         wd = self.app.wd
