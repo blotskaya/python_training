0b99fe253c4b7e05b87fd7a2f8a0ed45d6ecb41b
a.blotskaya <blotskaya.arina@gmail.com>
2022-10-27 23:33:29 +0300

    Добавлена проверка текущего состояния сессии

--- test/test_add_group_2_0.py
+++ test/test_add_group_2_0.py
@@ -2,6 +2,6 @@
 from model.group import Group
 
 def test_untitled_test_case(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="group_name1", header="group_header2", footer="group_footer3"))
-    app.session.logout()
+
+
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,7 +2,6 @@ from model.contact import Contact
 
 
 def test_edit_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(name="Jane", middlename="Lane", surname="Robert",
                                nickname="janelane", title="title", company="New Company",
                                address="Lomonosova 28", phonehome="89991112233", phonemobile="87771112233",
@@ -11,4 +10,3 @@ def test_edit_contact(app):
                                byear="1977", ayear="2015", address2="Moscow",
                                home2="Lenina 25", notes="otes", bday="\"9\"",
                                bmonth="\"October\"", aday="\"12\"", amonth="\"november\""))
-    app.session.logout()
