7299ec7ae5b55a2c783fc74be5c9b13e7a973fa7
a.blotskaya <blotskaya.arina@gmail.com>
2022-10-28 17:49:12 +0300

    Добавление проверки наличия группы

--- fixture/group.py
+++ fixture/group.py
@@ -52,6 +52,11 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
 
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
+
     def return_to_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
