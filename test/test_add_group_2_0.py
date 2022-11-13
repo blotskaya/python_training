ba70783758a75e54816cf7492d31f1e9a28d72cd
a.blotskaya <blotskaya.arina@gmail.com>
2022-11-13 20:11:15 +0300

    Параметризованы тесты для создания групп и контактов

--- test/test_add_group_2_0.py
+++ test/test_add_group_2_0.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import random
+import string
 
-def test_untitled_test_case(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+    for i in range(2)
+]
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_untitled_test_case(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="group_name1", header="group_header2", footer="group_footer3")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
--- test/test_create_contact_2.py
+++ test/test_create_contact_2.py
@@ -1,17 +1,28 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
-    
-def test_createcontact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Contact(name=random_string("name", 10), middlename=random_string("middlename", 8),
+                    surname=random_string("surname", 10), nickname=random_string("nickname", 7),
+                    title=random_string("title", 11), company=random_string("company", 12),
+                    address=random_string("address", 20), phonehome=random_string("phonehome", 9),
+                    phonemobile=random_string("phonemobile", 10), phonework=random_string("phonework", 6),
+                    phonefax=random_string("phonefax", 12), mail1=random_string("mail1", 20),
+                    mail2=random_string("mail2", 11), mail3=random_string("mail3", 15), hp=random_string("hp", 10),
+                    byear="1989", ayear="2020", address2=random_string("address2", 12),
+                    phonehome2=random_string("phonehome2", 11), notes=random_string("notes", 6),
+                    bday='"11"', bmonth='"December"', aday='"7"', amonth='"October"')
+            for i in range(2)]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_createcontact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(name="Maria", middlename="Kate", surname="White",
-                               nickname="mariakate", title="new_title", company="Big Company",
-                               address="Moscow, Lenina 24", phonehome="Lenina 24", phonemobile="89991112223",
-                               phonework="Big Company", phonefax="112233", mail1="mariakate@mail.com",
-                               mail2="bigcompany@mail.com", mail3="work@mail.com", hp="mariakate.com",
-                               byear="1989", ayear="2020", address2="Saint Petersburg",
-                               phonehome2="Lomonosova 25", notes="new_notes", bday='"11"',
-                               bmonth='"December"', aday='"7"', amonth='"October"')
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
