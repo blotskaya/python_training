b7071e8948a04e415bbe26544a951fd5c44e8ec2
a.blotskaya <blotskaya.arina@gmail.com>
2022-11-14 18:46:44 +0300

    Объединены тесты проверки контактов в один тест, исправлен мердж информации по имейлам

--- test/testcomparecontactinfo.py
+++ test/testcomparecontactinfo.py
@@ -3,7 +3,12 @@ import re
 def test_phones_on_homepage(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_home_page.name == contact_from_edit_page.name
+    assert contact_from_home_page.surname == contact_from_edit_page.surname
+    assert contact_from_home_page.address == contact_from_edit_page.address
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.all_email_from_home_page == merge_email_like_on_home_page(contact_from_edit_page)
+
 
 def clear(s):
     return re.sub("[() -]", "", s)
@@ -14,20 +19,8 @@ def merge_phones_like_on_home_page(contact):
                                 filter(lambda x: x is not None,
                                        [contact.phonehome, contact.phonemobile, contact.phonework, contact.phonehome2]))))
 
-def test_email_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.all_email_from_home_page == merge_email_like_on_home_page(contact_from_edit_page)
-
 def merge_email_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
-                                       [contact.mail1, contact.mail2, contact.mail3]))))
+                                       [contact.mail1, contact.mail2, contact.mail3])))
 
-def test_main_info_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.name == contact_from_edit_page.name
-    assert contact_from_home_page.surname == contact_from_edit_page.surname
-    assert contact_from_home_page.address == contact_from_edit_page.address
