c3d1def850c96475078cab4a19313882a281b643
a.blotskaya <blotskaya.arina@gmail.com>
2022-11-20 20:51:46 +0300

    Реализованы динамические генераторы тестовых данных для групп и контактов

---
+++ data/contacts.py
@@ -0,0 +1,10 @@
+from model.contact import Contact
+
+testdata = [Contact(name="testname", middlename="testmiddlname", surname="testsurname",
+                               nickname="testnickname", title="testtitle", company="testcompany",
+                               address="testaddress", phonehome="testphonehome", phonemobile="testphonemobile",
+                               phonework="testphonework", phonefax="testphonefax", mail1="testmail1",
+                               mail2="testmail2", mail3="testmail3", hp="testhp",
+                               byear="1980", ayear="2022", address2="testaddress2",
+                               phonehome2="testhome2", notes="testnotes", bday='"11"',
+                               bmonth='"September"', aday='"10"', amonth='"October"')]
--- test/test_create_contact_2.py
+++ test/test_create_contact_2.py
@@ -2,8 +2,8 @@
 from model.contact import Contact
 import pytest
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_createcontact(app, contact):
+def test_createcontact(app, data_contacts):
+    contact = data_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
