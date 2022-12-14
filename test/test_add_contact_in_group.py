4fcb028fb3e1dff62b519511c9176d89cff4e76e
a.blotskaya <blotskaya.arina@gmail.com>
2022-12-14 12:19:41 +0300

    Исправлена ошибка, если в тест добавления контакта в группу нет контактов не в группе

--- test/test_add_contact_in_group.py
+++ test/test_add_contact_in_group.py
@@ -21,6 +21,15 @@ def test_add_contact_in_group(app, db):
     selection_group = random.choice(old_groups)
     old_contacts_in_group = database.get_contacts_in_group(Group(id=selection_group.id))
     contacts_not_in_group = database.get_contacts_not_in_group(Group(id=selection_group.id))
+    if contacts_not_in_group == []:
+        app.contact.create((Contact(name="testname", middlename="testmiddlname", surname="testsurname",
+                                    nickname="testnickname", title="testtitle", company="testcompany",
+                                    address="testaddress", phonehome="testphonehome", phonemobile="testphonemobile",
+                                    phonework="testphonework", phonefax="testphonefax", mail1="testmail1",
+                                    mail2="testmail2", mail3="testmail3", hp="testhp",
+                                    byear="1980", ayear="2022", address2="testaddress2",
+                                    phonehome2="testhome2", notes="testnotes", bday='"11"',
+                                    bmonth='"September"', aday='"10"', amonth='"October"')))
     selection_contact = random.choice(contacts_not_in_group)
     app.contact.add_contact_in_group(selection_contact.id, selection_group.id)
     new_contacts_in_group = database.get_contacts_in_group(Group(id=selection_group.id))
